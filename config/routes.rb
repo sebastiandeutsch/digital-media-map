8cb786dbaed4d2c4b54011e0f79efd4e4164bd5c
Sebastian Deutsch <sebastian.deutsch@9elements.com>
2012-04-28 12:44:08 +0200

    Added about page

--- config/routes.rb
+++ config/routes.rb
@@ -51,7 +51,8 @@ DigitalMediaMap::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root to: "home#index"
+  match 'about' => 'pages#about'
+  root to: "home#index"
 
   # See how all your routes lay out with "rake routes"
 
