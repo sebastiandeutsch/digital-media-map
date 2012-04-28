de228dd5efa39787db0f59c63908f68065acda72
Sebastian Deutsch <sebastian.deutsch@9elements.com>
2012-04-28 19:51:25 +0200

    Added logo to schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120428150758) do
+ActiveRecord::Schema.define(:version => 20120428171425) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(:version => 20120428150758) do
     t.float    "lat"
     t.datetime "created_at",           :null => false
     t.datetime "updated_at",           :null => false
+    t.string   "logo"
   end
 
   create_table "taggings", :force => true do |t|
