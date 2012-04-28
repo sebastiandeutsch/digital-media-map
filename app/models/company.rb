c61af7a7026594f18be86877c08931d6095333fa
Sebastian Deutsch <sebastian.deutsch@9elements.com>
2012-04-28 21:31:43 +0200

    Added logo to mass assignment

--- app/models/company.rb
+++ app/models/company.rb
@@ -5,7 +5,7 @@ class Company < ActiveRecord::Base
 
   mount_uploader :logo, LogoUploader
 
-  attr_accessible :name, :description, :street, :zip, :city, :url, :email, :searches, :provides, :facebook_url, :twitter_url, :rss_url
+  attr_accessible :name, :description, :street, :zip, :city, :url, :email, :searches, :provides, :facebook_url, :twitter_url, :rss_url, :logo
 
   before_save :query_for_lonlat
   
