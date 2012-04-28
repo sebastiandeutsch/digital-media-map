a68827d12560fd276e1dfb44109392d6cd840326
rubiii <me@rubiii.com>
2012-04-28 16:00:29 +0200

    rm test for removed feature

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -5,14 +5,4 @@ describe Company do
   it { should belong_to :category }
   it { should have_and_belong_to_many :tags }
 
-
-  describe "Tag names" do
-
-    subject {
-      company = Factory(:company)
-      company.tags = [Tag.create(:name => "foo")]
-      company.save
-    }
-    its(:tag_names) { should eql(["foo"])}
-  end
 end
