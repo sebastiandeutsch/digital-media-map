6492221a056fec069be1015989f6badd6e5cc439
Sebastian Deutsch <sebastian.deutsch@9elements.com>
2012-04-28 13:20:34 +0200

    Added company controller

---
+++ app/controllers/companies_controller.rb
@@ -0,0 +1,44 @@
+class CompaniesController < ApplicationController
+  def index
+    @companies = Company.all
+  end
+  
+  def show
+    @company = Company.find(params[:id])
+  end
+  
+  def new
+    @company = Company.new
+  end
+  
+  def edit
+    @company = Company.find(params[:id])
+  end
+  
+  def create
+    @company = Company.new(params[:company])
+    if @company.save()
+      redirect_to :back, :notice => 'Company updated'
+    else
+      redirect_to :back, :error => 'Could not update Company'
+    end
+  end
+  
+  def update
+    @company = Company.find(params[:id])
+    if @company.update_attributes(params[:company])
+      redirect_to :back, :notice => 'Company updated'
+    else
+      redirect_to :back, :error => 'Could not update Company'
+    end
+  end
+  
+  def destroy
+    @company = Company.find(params[:id])
+    if @company.destroy
+      redirect_to :back, :notice => 'Company deleted'
+    else
+      redirect_to :back, :error => 'Could not delete Company'
+    end
+  end
+end
