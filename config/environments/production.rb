e56ac2f913685a13f649f68618fc411047a2d200
Michael Bumann <michael@railslove.com>
2012-04-29 12:44:54 +0200

    lovely activeadmin assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ DigitalMediaMap::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
-  config.assets.precompile += %w[active_admin.css active_admin.js]
+  config.assets.precompile += %w[active_admin.css active_admin.js active_admin/print.css]
   
   # Defaults to Rails.root.join("public/assets")
   # config.assets.manifest = YOUR_PATH
