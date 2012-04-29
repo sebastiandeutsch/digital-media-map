fe74bea878ba26fd8f03fdb961ddddcd03c65954
rubiii <me@rubiii.com>
2012-04-29 12:09:42 +0200

    allow http connections when vcr is not used

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,7 +18,8 @@ RSpec.configure do |config|
   config.after                  { DatabaseCleaner.clean }
 end
 
-VCR.configure do |c|
-  c.cassette_library_dir = 'spec/cassettes'
-  c.hook_into :webmock
+VCR.configure do |config|
+  config.cassette_library_dir = 'spec/cassettes'
+  config.hook_into :webmock
+  config.allow_http_connections_when_no_cassette = true
 end
