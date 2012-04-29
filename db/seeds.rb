f751f8e66df3c63df83700ccf458f34df7d6b70a
Michael Bumann <michael@railslove.com>
2012-04-29 10:03:29 +0200

    category seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,37 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Emanuel', :city => cities.first)
 
+
+
+{
+  "Advertising" => [
+      
+  ],
+  "eCommerce / eTransaction" => [
+  
+  ], 
+  "Games" => [
+  
+  ],
+  "Web & App Agency" => [
+  
+  ],
+  "Content" => [
+  
+  ], 
+  "Social Networks" => [
+  
+  ],
+  "Web Tools / Services" "Others" => [
+  
+  ]
+}.each do |category_name, tags|
+  c = Category.create :name => category_name
+  tags.each do |tag_name|
+    c.tags << Tag.create(:name => tag_name)
+  end
+end
+
 companies = YAML.load File.open(File.join(Rails.root, "db/seed.yml"))
 companies.each do |company|
   tags = company.delete("sub_categories")
